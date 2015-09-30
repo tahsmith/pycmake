58ca60d5ea1dbe83f771584534f10ec53965f44c
Tim Smith <tahs88@gmail.com>
2015-09-30 22:05:08 +1000

    typo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pycmake',
+    version='',
+    packages=['cmake', 'cmake.elements'],
+    url='',
+    license='',
+    author='Timothy A H Smith',
+    author_email='tahs88@gmail.com',
+    description=''
+)
